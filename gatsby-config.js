52c354367f127674df73109eb175895dad1585b0
jkahne <jkahne@gmail.com>
2019-11-14 07:51:59 -0500

    add sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,47 @@ module.exports = {
     title: `Nimble River`,
     author: `Jeremy Kahne`,
     description: ``,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://sad-goldwasser-171981.netlify.com/`,
     social: {
       twitter: `jeremykahne`,
     },
   },
   plugins: [
     `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: `/sitemap.xml`,
+        // Exclude specific pages or groups of pages using glob parameters
+        // See: https://github.com/isaacs/minimatch
+        // The example below will exclude the single `path/to/page` and all routes beginning with `category`
+        // exclude: ["/category/*", `/path/to/page`],
+        query: `
+          {
+            site {
+              siteMetadata {
+                siteUrl
+              }
+            }
+
+            allSitePage {
+              edges {
+                node {
+                  path
+                }
+              }
+            }
+        }`,
+        // serialize: ({ site, allSitePage }) =>
+        //   allSitePage.edges.map(edge => {
+        //     return {
+        //       url: site.siteMetadata.siteUrl + edge.node.path,
+        //       changefreq: `daily`,
+        //       priority: 0.7,
+        //     }
+        //   })
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
